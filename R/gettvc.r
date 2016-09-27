002ecf678615d85a94b90b5abc87156aec0fb769
swihart <bruce.swihart@gmail.com>
2016-09-27 13:11:16 -0400

    initial commit 2.0

---
+++ R/gettvc.r
@@ -0,0 +1,127 @@
+#
+#  rmutil : A Library of Special Functions for Repeated Measurements
+#  Copyright (C) 1998, 1999, 2000, 2001 J.K. Lindsey
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public Licence as published by
+#  the Free Software Foundation; either version 2 of the Licence, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public Licence for more details.
+#
+#  You should have received a copy of the GNU General Public Licence
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#
+#  SYNOPSIS
+#
+#     gettvc(response, times=NULL, tvcov=NULL, tvctimes=NULL,
+#	oldtvcov=NULL, ties=TRUE)
+#
+#  DESCRIPTION
+#
+#    Function to find the most recent value of a time-varying
+# covariate not recorded at the same time as the response.
+
+gettvc <- function(response, times=NULL, tvcov=NULL, tvctimes=NULL,
+	oldtvcov=NULL, ties=TRUE){
+#
+# if necessary, make into response object and remove NAs
+#
+if(inherits(response,"response"))izr <- response
+else izr <- restovec(response,times)
+if(any(is.na(izr$y))){
+	isna <- TRUE
+	nna <- length(izr$y)
+	tmp <- NULL
+	rna <- !is.na(izr$y)
+	irna <- (1:nna)*rna
+	j <- c(0,cumsum(nobs(izr)))
+	for(i in 1:length(nobs(izr)))tmp <- c(tmp,sum(rna[(j[i]+1):j[i+1]]))
+	zr <- list()
+	zr$nobs <- tmp
+	zr$times <- izr$times[rna]
+	zr$y <- izr$y[rna]
+	class(zr) <- "response"}
+else {
+	isna <- FALSE
+	zr <- izr}
+#
+# remove NAs from time-varying covariate
+#
+zt <- restovec(tvcov,tvctimes)
+if(any(is.na(zt$y))){
+	tmp <- NULL
+	rna <- !is.na(zt$y)
+	j <- c(0,cumsum(nobs(zt)))
+	for(i in 1:length(nobs(zt)))tmp <- c(tmp,sum(rna[(j[i]+1):j[i+1]]))
+	zt$nobs <- tmp
+	zt$times <- zt$times[rna]
+	zt$y <- zt$y[rna]}
+if(length(nobs(izr))!=length(nobs(zt)))
+	stop("response and covariate do not have the same number of individuals")
+#
+# obtain new aligned times
+#
+nind <- length(nobs(zr))
+nld <- max(c(nobs(zr),nobs(zt)))
+z2 <- .Fortran("gettvc",
+	x=as.double(zr$times),
+	y=as.double(zr$y),
+	xtvc=as.double(zt$times),
+	tvcov=as.double(zt$y),
+	nobs=as.integer(nobs(zr)),
+	nind=as.integer(nind),
+	nknt=as.integer(nobs(zt)),
+	ties=as.logical(ties),
+	xu=matrix(0,nrow=nind,ncol=2*nld),
+	ndelta=logical(2*nld*nind),
+	tvcov2=matrix(0,nrow=nind,ncol=2*nld),
+	nu=integer(nind),
+	wu=double(2*nld),
+	nld=as.integer(nld),
+	tvcov3=double(length(zr$y)),
+	DUP=FALSE,
+	PACKAGE="rmutil")
+if(isna){
+	tvcov3 <- rep(NA,nna)
+	tvcov3[irna] <- z2$tvcov3}
+else tvcov3 <- z2$tvcov3
+rm(z2)
+#
+# check if new covariate or to be combined with others
+#
+new <- missing(oldtvcov)
+if(!new&!is.list(oldtvcov)){
+	warning("oldtvcov must form a list - ignored")
+	new <- TRUE}
+cname <- paste(deparse(substitute(tvcov)))
+if(new)oldtvcov <- vector(mode="list",nind)
+else if(!inherits(oldtvcov,"tvcov")){
+	if(length(oldtvcov)!=nind)
+		stop(paste("Previous time-varying covariate list must have length",nind))
+	else if(!is.null(colnames(oldtvcov[[1]])))
+		cname <- c(colnames(oldtvcov[[1]]),cname)
+	else cname <- NULL}
+else if(inherits(oldtvcov,"tvcov"))cname <- c(colnames(oldtvcov$tvcov),cname)
+if(inherits(oldtvcov,"tvcov")){
+#
+# combine tvcov objects
+#
+	oldtvcov$tvcov <- cbind(oldtvcov$tvcov,tvcov3)
+	colnames(oldtvcov$tvcov) <- cname}
+else {
+#
+# create a new tvcov object
+#
+	nm <- 0
+	for(i in 1:nind){
+		oldtvcov[[i]] <- cbind(oldtvcov[[i]],tvcov3[(nm+1):
+			(nm+nobs(izr)[i])])
+		nm <- nm+nobs(izr)[i]
+		if(!is.null(cname))colnames(oldtvcov[[i]]) <- cname}
+	oldtvcov <- tvctomat(oldtvcov)}
+invisible(oldtvcov)}
---
+++ R/util.r
@@ -0,0 +1,93 @@
+#
+#  rmutil : A Library of Special Functions for Repeated Measurements
+#  Copyright (C) 1998, 1999, 2000, 2001 J.K. Lindsey
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public Licence as published by
+#  the Free Software Foundation; either version 2 of the Licence, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public Licence for more details.
+#
+#  You should have received a copy of the GNU General Public Licence
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#
+#  SYNOPSIS
+#
+#     wr(formula, data=NULL, expand=T))
+#     det(x)
+#     capply(x, index, fcn=sum)
+#     mexp(x, t=1, n=20, k=3)
+#     contr.mean(n, contrasts=TRUE)
+#
+#  DESCRIPTION
+#
+#    Utility functions for repeated measurements
+
+### function to find the response vector and design matrix from a W&R formula
+###
+wr <- function(formula, data=NULL, expand=TRUE){
+if(is.null(data))data <- parent.frame()
+else if(!is.data.frame(data)&&!is.environment(data))
+	data <- if(expand||inherits(data,"tccov"))as.data.frame(data)
+		else as.data.frame(data$ccov)
+mt <- terms(formula)
+mf <- model.frame(mt,data=data,na.action=NULL)
+list(response=model.response(mf,"numeric"), design=model.matrix(mt,mf))}
+
+### a fast simplified version of tapply
+###
+capply <- function(x, index, fcn=sum){
+ans <- NULL
+for(i in split(x,index))ans <- c(ans,fcn(i))
+ans}
+
+### matrix exponentiation
+###
+mexp <- function(x, t=1, type="spectral decomposition", n=20, k=3){
+if(!is.matrix(x))stop("x must be a matrix")
+if(dim(x)[1]!=dim(x)[2])stop("x must be a square matrix")
+type <- match.arg(type,c("spectral decomposition","series approximation"))
+if(type=="spectral decomposition"){
+	z <- eigen(t*x,sym=FALSE)
+	p <- z$vectors%*%diag(exp(z$values))%*%solve(z$vectors)}
+else {
+	xx <- x*t/2^k
+	p <- diag(dim(x)[2])
+	q <- p
+	for(r in 1:n){
+		q <- xx%*%q/r
+		p <- p+q}
+	for(i in 1:k) p <- p%*%p}
+p}
+
+### matrix power
+###
+"%^%" <- function(x, p){
+if(!is.matrix(x))stop("x must be a matrix")
+if(dim(x)[1]!=dim(x)[2])stop("x must be a square matrix")
+z <- eigen(x,sym=FALSE)
+z$vectors%*%diag(z$values^p)%*%solve(z$vectors)}
+
+#    A function to provide correct constraints about the mean
+#  (correcting contr.sum)
+
+contr.mean <- function(n, contrasts=TRUE){
+if(length(n) <= 1){
+	if(is.numeric(n)&&length(n)==1&&n>1)levels <- 1:n
+	else stop("Not enough degrees of freedom to define contrasts")}
+else levels <- n
+lenglev <- length(levels)
+if(contrasts){
+	cont <- array(0,c(lenglev,lenglev-1),
+		list(levels,levels[1:(lenglev-1)]))
+	cont[col(cont)==row(cont)] <- 1
+	cont[lenglev,] <- -1}
+else {
+	cont <- array(0,c(lenglev,lenglev),list(levels,levels))
+	cont[col(cont) == row(cont)] <- 1}
+cont}
